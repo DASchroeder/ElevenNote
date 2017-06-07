c31dc95ca9998a79013f93eebdff318cd7985d3c
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-06-07 12:23:54 -0400

    layout and styling changes

---
+++ ElevenNote.Web/CommonController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ElevenNote.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace ElevenNote.Web 
+{
+    public class CommonController : Controller
+    {
+        #region Properties
+
+        private ApplicationSignInManager __signInManager;
+        private ApplicationUserManager _userManager;
+
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return __signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            protected set
+            {
+                __signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            protected set
+            {
+                _userManager = value;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the currently logged in User ID, likely populated by the Bearer token when authenticating.
+        /// </summary>
+        protected Guid AuthUserId
+        {
+            get
+            {
+                return new Guid(User.Identity.GetUserId());
+
+            }
+        }
+
+        #endregion
+
+    }
+}
